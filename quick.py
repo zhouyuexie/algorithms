d3009cf5a68f2ee4174a4802ccb77b93cfd1eae6
zhouyuexie <zhouyuexie@hotmail.com>
2016-04-09 08:32:16 +0800

    修改注释

--- quick.py
+++ quick.py
@@ -13,22 +13,22 @@ def quicksort(array,low,hign):
 	i = low
 	j = hign
 	if(i>=j):
-		return array
+		return array# 数组只剩一个元素或者没有元素
 	keyvalue = array[low]# 基准数
 	while(i!=j):
 		while(array[j]>=keyvalue and i<j):
-			j = j-1
+			j = j-1# 没有找到比基准数小的元素就一直找
 		while(array[i]<=keyvalue and i<j):
-			i = i+1
+			i = i+1# 没有找到比基准数大的元素就一直找
 
-		if(i<j):
+		if(i<j):# 交换i，j两个位置的元素
 			t = array[i]
 			array[i] = array[j]
 			array[j] = t
-	array[low] = array[i]
-	array[i] = keyvalue# 将基数换到i=j的位置
-	quicksort(array,low,i-1)
-	quicksort(array,j+1,hign)
+	array[low] = array[i]# 将基数换到i=j的位置
+	array[i] = keyvalue# 将i=j位置的元素换到基数位置
+	quicksort(array,low,i-1)# 递归运算左边数组，i=j的元素已经排好
+	quicksort(array,j+1,hign)# 递归运算右边数组
 	return array
 
 start = datetime.now().microsecond# 开始时间
